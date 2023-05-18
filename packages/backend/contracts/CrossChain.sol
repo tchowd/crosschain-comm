ae1cb4bd881626fe71ee72c3c6836bf987439b24
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2023-05-18 03:41:04 -0400

    initial setup w/contract

---
+++ packages/backend/contracts/CrossChain.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.17;
+
+// import "https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/lzApp/NonblockingLzApp.sol";
+import './LzApp.sol';
+import './NonblockingLzApp.sol';
+
+contract CrossChain is NonblockingLzApp {
+    string public data = "Nothing received yet";
+    uint16 destChainId;
+    
+    constructor(address _lzEndpoint) NonblockingLzApp(_lzEndpoint) {
+        if (_lzEndpoint == 0xae92d5aD7583AD66E49A0c67BAd18F6ba52dDDc1) destChainId = 10121;
+        if (_lzEndpoint == 0xbfD2135BFfbb0B5378b56643c2Df8a87552Bfa23) destChainId = 10132;
+    }
+
+    function _nonblockingLzReceive(uint16, bytes memory, uint64, bytes memory _payload) internal override {
+       data = abi.decode(_payload, (string));
+    }
+
+    function send(string memory _message) public payable {
+        bytes memory payload = abi.encode(_message);
+        _lzSend(destChainId, payload, payable(msg.sender), address(0x0), bytes(""), msg.value);
+    }
+
+    function trustAddress(address _otherContract) public onlyOwner {
+        trustedRemoteLookup[destChainId] = abi.encodePacked(_otherContract, address(this));   
+    }
+
+    function getTrustedAddress() public view returns (address) {
+        (address trustedAddress,) = abi.decode(trustedRemoteLookup[destChainId], (address, address));
+        return trustedAddress;
+    }
+}

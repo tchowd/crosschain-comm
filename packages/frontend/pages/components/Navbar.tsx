15df4d74d3619d9eb99287cb4f9fed3d5d750a2f
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2023-05-20 02:24:07 -0400

    testing for rust

--- packages/frontend/pages/components/Navbar.tsx
+++ packages/frontend/pages/components/Navbar.tsx
@@ -9,10 +9,7 @@ import {
     useColorMode,
     Center,
   } from '@chakra-ui/react';
-  import {
-    HamburgerIcon,
-    CloseIcon,
-  } from '@chakra-ui/icons';
+
   
   export default function Navbar() {
     const { isOpen, onToggle } = useDisclosure();
@@ -26,20 +23,6 @@ import {
           px={{ base: 4 }}
           borderBottom={1}
           align={'center'}>
-          <Flex
-            flex={{ base: 1, md: 'auto' }}
-            ml={{ base: -2 }}
-            display={{ base: 'flex', md: 'none' }}>
-  
-            <IconButton
-              onClick={onToggle}
-              icon={
-                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-              }
-              variant={'ghost'}
-              aria-label={'Toggle Navigation'} />
-  
-          </Flex>
   
           <Flex flex={{ base: 1 }} justify={{ base: 'center' }}>
             <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
